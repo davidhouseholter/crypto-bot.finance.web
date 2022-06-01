fb4a2e6da93a8407a005a96c955313e03721bb18
David Householter <householter.david@gmail.com>
2022-06-01 09:34:47 -0600

    kratos config change

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,5 +1,8 @@
 import { Configuration, V0alpha2Api } from '@ory/client'
+import { edgeConfig } from '@ory/integrations/next'
 
+// export default new V0alpha2Api(new Configuration(edgeConfig))
+// console.log(process.env.ORY_SDK_URL)
 export default new V0alpha2Api(new Configuration({
     basePath: process.env.ORY_SDK_URL,
     baseOptions: {
--- pkg/services/userService.ts
+++ pkg/services/userService.ts
@@ -1,21 +1,7 @@
 import axios, { AxiosError } from 'axios';
-import { useRouter } from 'next/router';
-import { createContext, useContext, useEffect, useState } from 'react';
 import ory from '../sdk'
 
-// export default  class UserService {
-//     static currentSession: any = null;
-//     constructor() {
-//     }
-//     static getUser(): Promise<any> {
-//         if (this.currentSession != null) {
-//             return this.currentSession;
-//         }
-       
-//     }
-// }
 const api = process.env.NEXT_PUBLIC_API_ENDPOINT;
-const idapi = process.env.NEXT_PUBLIC_AUTH_URL;
 
 export function getUseProfile() {
     return axios.get(`${api}/user`,{withCredentials: true})
@@ -28,38 +14,31 @@ export function getUseProfile() {
 }
 
 export function getUserInformation() {
-  return axios.get(`${idapi}/sessions/whoami`,{withCredentials: true})
-  .then(function (response) {
-    return response.data;
-  })
-  .catch(function (error) {
-    return error.data;
-  });
-    // return new Promise((resolve) => {
-    //     return ory
-    //     .toSession()
-    //     .then(({ data }) => {
-    //         resolve(data)
-    //     })
-    //     .catch((err: AxiosError) => {
-    //         console.log(err)
+    return new Promise((resolve) => {
+        return ory
+        .toSession()
+        .then(({ data }) => {
+            resolve(data)
+        })
+        .catch((err: AxiosError) => {
+            console.log(err)
 
-    //         switch (err.response?.status) {
-    //             case 403:
-    //             // This is a legacy error code thrown. See code 422 for
-    //             // more details.
-    //             case 422:
-    //             // This status code is returned when we are trying to
-    //             // validate a session which has not yet completed
-    //             // it's second factor
-    //             // return router.push('/login?aal=aal2')
-    //             case 401:
-    //                 // do nothing, the user is not logged in
-    //                 return
-    //         }
+            switch (err.response?.status) {
+                case 403:
+                // This is a legacy error code thrown. See code 422 for
+                // more details.
+                case 422:
+                // This status code is returned when we are trying to
+                // validate a session which has not yet completed
+                // it's second factor
+                // return router.push('/login?aal=aal2')
+                case 401:
+                    // do nothing, the user is not logged in
+                    return
+            }
     
-    //         // Something else happened!
-    //         return Promise.reject(err)
-    //     })
-    // })
+            // Something else happened!
+            return Promise.reject(err)
+        })
+    })
   }
