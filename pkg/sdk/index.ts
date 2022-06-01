934b7788bb343c57b2250eb2171691c44866d71c
David Householter <householter.david@gmail.com>
2022-06-01 10:04:45 -0600

    hard code url

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,10 +1,11 @@
 import { Configuration, V0alpha2Api } from '@ory/client'
 import { edgeConfig } from '@ory/integrations/next'
+const api = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
 // export default new V0alpha2Api(new Configuration(edgeConfig))
 // console.log(process.env.ORY_SDK_URL)
-export default new V0alpha2Api(new Configuration({
-    basePath: process.env.ORY_SDK_URL,
+export default new V0alpha2Api(new Configuration({...edgeConfig,
+    basePath: "https://id.crypto-bot.finance",
     baseOptions: {
       withCredentials: true,
     }
