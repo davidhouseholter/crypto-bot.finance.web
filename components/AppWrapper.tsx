109c7db25ef47469d5e598eb62c39dbcb4689a6d
David Householter <householter.david@gmail.com>
2022-05-31 18:05:03 -0600

    fix timming issue

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -21,6 +21,7 @@ export function AppWrapper({ children }: AppWrapperProps): JSX.Element {
     hasSession: false,
     currentUser: null,
     lastCheck: null,
+    profileCheck: false
   });
   const [userProfile, setUserProfile] = useState<any>(null);
 
@@ -32,11 +33,17 @@ export function AppWrapper({ children }: AppWrapperProps): JSX.Element {
           hasSession: user == null ? false : true,
           currentUser: user,
           lastCheck: true,
+          profileCheck: false
         };
         setUserIdentity(sharedState as any);
         dispatch(changeUserIdentityState(sharedState));
       });
 
+     
+    }
+    if(userIdentity?.hasSession && userProfile == null && !userIdentity.profileCheck) {
+      setUserIdentity({...userIdentity, profileCheck:true});
+      dispatch(changeUserIdentityState(userIdentity));
       getUseProfile().then((profile: any) => {
         setUserProfile(profile);
         dispatch(changeUserProfileState(profile));
