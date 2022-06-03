cdaedb29dc28f49ebc4067645fc28c96a5b6f681
David Householter <householter.david@gmail.com>
2022-06-02 21:03:47 -0600

    prod build

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -2,7 +2,8 @@ import { Configuration, V0alpha2Api } from '@ory/client'
 import { edgeConfig } from '@ory/integrations/next'
 const api = process.env.NEXT_PUBLIC_API_ENDPOINT;
 let config = new Configuration(edgeConfig);
-if(process.env.PRODUCTION){
+if(true){
+//if(process.env.PRODUCTION){
     config = new Configuration({...edgeConfig,
         basePath: "https://id.crypto-bot.finance",
         baseOptions: {
